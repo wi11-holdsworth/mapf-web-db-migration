0940133c2b946d4ff426a6c920a1528680c7e786
Will Holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2024-07-16 03:51:10 +1000

    uncomment

--- bin/main.sh
+++ bin/main.sh
@@ -6,13 +6,12 @@ source .env
 set +a
 
 # we only want to dump small collections
-echo $MONGO_SMALL_COLLECTIONS
-# IFS=':' read -ra collections <<< "$MONGO_SMALL_COLLECTIONS"
+IFS=':' read -ra collections <<< "$MONGO_SMALL_COLLECTIONS"
 
-# # dump all records from each collection
-# for collection in "${collections[@]}"; do
-#     mongodump --uri=$MONGO_OLD_URI --db=$MONGO_OLD_DB -c=$collection
-# done
+# dump all records from each collection
+for collection in "${collections[@]}"; do
+    mongodump --uri=$MONGO_OLD_URI --db=$MONGO_OLD_DB -c=$collection
+done
 
 # # upload all small collections
 # mongorestore --uri=$MONGO_NEW_URI --nsFrom="$MONGO_OLD_DB.*" --nsTo="$MONGO_NEW_DB.*"
